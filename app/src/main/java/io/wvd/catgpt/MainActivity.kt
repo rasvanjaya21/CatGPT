648721edf83450df9623e80dbd95e4bf3dd05902
Yusril A <mrsejati17@gmail.com>
2023-03-02 00:55:06 +0700

    add delay for splash n @AndroidEntryPoint

--- app/src/main/java/io/wvd/catgpt/MainActivity.kt
+++ app/src/main/java/io/wvd/catgpt/MainActivity.kt
@@ -1,6 +1,8 @@
 package io.wvd.catgpt
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,35 +14,22 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import dagger.hilt.android.AndroidEntryPoint
+import io.wvd.catgpt.ui.screen.LayoutScreen
 import io.wvd.catgpt.ui.theme.CatGPTTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        installSplashScreen()
+        var keepSplashOnScreen = true
+        val delay = 2000L
+        installSplashScreen().setKeepOnScreenCondition { keepSplashOnScreen }
+        Handler(Looper.getMainLooper()).postDelayed( { keepSplashOnScreen = false }, delay)
         super.onCreate(savedInstanceState)
         setContent {
             CatGPTTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.primary
-                ) {
-                    Greeting("Meow")
-                }
+                LayoutScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!", color = Color.White)
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    CatGPTTheme {
-        Greeting("Android")
     }
 }
